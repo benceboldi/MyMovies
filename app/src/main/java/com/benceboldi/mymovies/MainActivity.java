847e125e101e748acfe4bf5ef6b5af9ab35fb657
benceboldi <51209057+benceboldi@users.noreply.github.com>
2020-05-31 16:56:40 +0200

    Implemented the Cardview in MainActivity with MyHolder

--- app/src/main/java/com/benceboldi/mymovies/MainActivity.java
+++ app/src/main/java/com/benceboldi/mymovies/MainActivity.java
@@ -1,32 +1,61 @@
 package com.benceboldi.mymovies;
 
-import android.os.Bundle;
-import android.widget.Toast;
-
-import com.google.android.material.bottomnavigation.BottomNavigationView;
-
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.navigation.NavController;
-import androidx.navigation.Navigation;
-import androidx.navigation.ui.AppBarConfiguration;
-import androidx.navigation.ui.NavigationUI;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.firebase.ui.database.FirebaseRecyclerAdapter;
+import com.firebase.ui.database.FirebaseRecyclerOptions;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.squareup.picasso.Picasso;
 
 public class MainActivity extends AppCompatActivity {
+
+    private DatabaseReference reference;
+    private RecyclerView recyclerView;
+
+    private FirebaseRecyclerOptions<Movies> options;
+    private FirebaseRecyclerAdapter<Movies, MyHolder> adapter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        BottomNavigationView navView = findViewById(R.id.nav_view);
-        // Passing each menu ID as a set of Ids because each
-        // menu should be considered as top level destinations.
-        AppBarConfiguration appBarConfiguration = new AppBarConfiguration.Builder(
-                R.id.navigation_home, R.id.navigation_dashboard, R.id.navigation_notifications)
-                .build();
-        NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment);
-        NavigationUI.setupActionBarWithNavController(this, navController, appBarConfiguration);
-        NavigationUI.setupWithNavController(navView, navController);
-        Toast.makeText(MainActivity.this, "Firebase connected!", Toast.LENGTH_LONG).show();
-    }
 
+        reference = FirebaseDatabase.getInstance().getReference();
+
+        recyclerView = findViewById(R.id.recyclerView);
+        recyclerView.setHasFixedSize(true);
+        recyclerView.setLayoutManager(new LinearLayoutManager(this));
+
+        options = new FirebaseRecyclerOptions.Builder<Movies>().setQuery(reference, Movies.class).build();
+        adapter = new FirebaseRecyclerAdapter<Movies, MyHolder>(options) {
+            @Override
+            protected void onBindViewHolder(@NonNull MyHolder holder, int position, @NonNull Movies model) {
+                holder.mTitle.setText(model.getTitle());
+                Picasso.get().load(model.getImg()).into(holder.mImageView);
+                holder.mDes.setText(model.getDescription());
+            }
+
+            @NonNull
+            @Override
+            public MyHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+
+                View v = LayoutInflater.from(parent.getContext()).inflate(R.layout.row, parent, false);
+                return new MyHolder(v);
+            }
+        };
+
+        adapter.startListening();
+        recyclerView.setAdapter(adapter);
+
+
+    }
 }
---
+++ app/src/main/java/com/benceboldi/mymovies/MyHolder.java
@@ -0,0 +1,22 @@
+package com.benceboldi.mymovies;
+
+import android.view.View;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+public class MyHolder extends RecyclerView.ViewHolder {
+
+    ImageView mImageView;
+    TextView mTitle, mDes;
+
+    public MyHolder(@NonNull View itemView){
+        super(itemView);
+
+        mImageView = itemView.findViewById(R.id.imageId);
+        mTitle = itemView.findViewById(R.id.titleId);
+        mDes = itemView.findViewById(R.id.descId);
+    }
+}
